6493f9b3be31e15df826dd8cf73836d55acfebfd
Michal Dvorak <michal@mdvorak.org>
2020-06-02 22:21:46 +0200

    no need to init nvs (done by Arduino FW), release BT memory

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <esp_https_ota.h>
 #include <nvs_flash.h>
 #include <nvs.h>
+#include <esp_bt.h>
 #include <EspWifiSetup.h>
 #include "version.h"
 
@@ -11,16 +12,8 @@ const auto MAIN_LOOP_INTERVAL = 1000;
 
 void setup()
 {
-  esp_err_t err;
-
-  // Initialize NVS
-  err = nvs_flash_init();
-  if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
-  {
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    err = nvs_flash_init();
-  }
-  ESP_ERROR_CHECK(err);
+  // Disable BT
+  ESP_ERROR_CHECK_WITHOUT_ABORT(esp_bt_mem_release(ESP_BT_MODE_BTDM));
 
   // WiFi
   WiFiSetup(WIFI_SETUP_WPS);
