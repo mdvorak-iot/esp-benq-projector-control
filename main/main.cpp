94b60b4e6bdb2534f79701e2f44fb07ed667d16e
Michal Dvorak <michal@mdvorak.org>
2021-04-03 16:29:01 +0200

    moved status led setup to own file

--- main/main.cpp
+++ main/main.cpp
@@ -7,23 +7,21 @@
 #include <mqtt_client.h>
 #include <nvs_flash.h>
 #include <status_led.h>
-#include <wps_config.h>
 
 static const char TAG[] = "main";
-static const auto STATUS_LED_CONNECTING_INTERVAL = 500u;
-static const auto STATUS_LED_WPS_INTERVAL = 100u;
 
 // Global state
 static hw_config config = {};
 static status_led_handle_ptr status_led = nullptr;
 static esp_mqtt_client_handle_t mqtt_client = nullptr;
-aws_iot_shadow_handle_ptr shadow_handle = nullptr;
+static aws_iot_shadow_handle_ptr shadow_handle = nullptr;
 
 static void setup_init();
 static void setup_devices();
 extern "C" void setup_wifi(const char *hostname);
 extern "C" void setup_wifi_start();
 extern "C" void setup_aws_iot(esp_mqtt_client_handle_t *out_mqtt_client, aws_iot_shadow_handle_ptr *out_shadow_client);
+extern "C" void setup_status_led(gpio_num_t status_led_pin, bool status_led_on_state, aws_iot_shadow_handle_ptr shadow_handle, status_led_handle_ptr *out_status_led);
 
 extern "C" void app_main()
 {
@@ -35,6 +33,7 @@ extern "C" void app_main()
     setup_devices();
     setup_wifi(app_info.project_name);
     setup_aws_iot(&mqtt_client, &shadow_handle);
+    setup_status_led(config.status_led_pin, config.status_led_on_state, shadow_handle, &status_led);
     setup_wifi_start(); // this should be last
     ESP_LOGI(TAG, "started %s %s", app_info.project_name, app_info.version);
 
@@ -43,26 +42,6 @@ extern "C" void app_main()
 }
 
 // Setup logic
-static void status_led_wps_event_handler(__unused void *arg, esp_event_base_t event_base,
-                                         int32_t event_id, __unused void *event_data)
-{
-    if (event_base == WIFI_EVENT
-        && (event_id == WIFI_EVENT_STA_WPS_ER_SUCCESS || event_id == WIFI_EVENT_STA_WPS_ER_TIMEOUT || event_id == WIFI_EVENT_STA_WPS_ER_FAILED))
-    {
-        status_led_set_interval(status_led, STATUS_LED_CONNECTING_INTERVAL, true);
-    }
-    else if (event_base == WPS_CONFIG_EVENT && event_id == WPS_CONFIG_EVENT_START)
-    {
-        status_led_set_interval(status_led, STATUS_LED_WPS_INTERVAL, true);
-    }
-}
-
-static void status_led_disconnected_handler(__unused void *arg, esp_event_base_t event_base,
-                                            __unused int32_t event_id, __unused void *event_data)
-{
-    status_led_set_interval(status_led, STATUS_LED_CONNECTING_INTERVAL, true);
-}
-
 static void setup_init()
 {
     // Initialize NVS
@@ -92,21 +71,6 @@ static void setup_init()
     if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND)
     {
         ESP_LOGE(TAG, "failed to load config: %d %s", err, esp_err_to_name(err));
-    }
-
-    // Status LED
-    err = status_led_create(config.status_led_pin, config.status_led_on_state, &status_led);
-    if (err == ESP_OK)
-    {
-        ESP_ERROR_CHECK_WITHOUT_ABORT(status_led_set_interval(status_led, STATUS_LED_CONNECTING_INTERVAL, true));
-
-        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, status_led_wps_event_handler, nullptr, nullptr));
-        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(WPS_CONFIG_EVENT, WPS_CONFIG_EVENT_START, status_led_wps_event_handler, nullptr, nullptr));
-        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, status_led_disconnected_handler, nullptr, nullptr));
-    }
-    else
-    {
-        ESP_LOGE(TAG, "failed to create status_led on pin %d: %d %s", config.status_led_pin, err, esp_err_to_name(err));
     }
 }
 
