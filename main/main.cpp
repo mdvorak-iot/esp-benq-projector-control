cb1ea2cfbe87a455bac935c22e50ffa6e200b14e
Michal Dvorak <michal@mdvorak.org>
2021-04-03 15:37:52 +0200

    refactoring of standard init

--- main/main.cpp
+++ main/main.cpp
@@ -1,15 +1,61 @@
+#include "device_state.h"
+#include "hw_config.h"
 #include <double_reset.h>
 #include <esp_log.h>
 #include <esp_ota_ops.h>
 #include <esp_wifi.h>
 #include <nvs_flash.h>
 #include <status_led.h>
-#include <wifi_reconnect.h>
 #include <wps_config.h>
 
 static const char TAG[] = "main";
+static const auto STATUS_LED_CONNECTING_INTERVAL = 500u;
+static const auto STATUS_LED_WPS_INTERVAL = 100u;
 
-static bool reconfigure = false;
+// Global state
+static status_led_handle_ptr status_led;
+static hw_config config = {};
+
+static void setup_init();
+static void setup_devices();
+extern "C" void setup_wifi(const char *hostname);
+static void setup_final(const esp_app_desc_t *app_info);
+
+extern "C" void app_main()
+{
+    esp_app_desc_t app_info = {};
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_ota_get_partition_description(esp_ota_get_running_partition(), &app_info));
+
+    // Setup
+    setup_init();
+    setup_devices();
+    setup_wifi(app_info.project_name);
+    setup_final(&app_info);
+
+    // Run
+    ESP_LOGI(TAG, "life is good");
+}
+
+// Setup logic
+static void status_led_wps_event_handler(__unused void *arg, esp_event_base_t event_base,
+                                         int32_t event_id, __unused void *event_data)
+{
+    if (event_base == WIFI_EVENT
+        && (event_id == WIFI_EVENT_STA_WPS_ER_SUCCESS || event_id == WIFI_EVENT_STA_WPS_ER_TIMEOUT || event_id == WIFI_EVENT_STA_WPS_ER_FAILED))
+    {
+        status_led_set_interval(status_led, STATUS_LED_CONNECTING_INTERVAL, true);
+    }
+    else if (event_base == WPS_CONFIG_EVENT && event_id == WPS_CONFIG_EVENT_START)
+    {
+        status_led_set_interval(status_led, STATUS_LED_WPS_INTERVAL, true);
+    }
+}
+
+static void status_led_disconnected_handler(__unused void *arg, esp_event_base_t event_base,
+                                            __unused int32_t event_id, __unused void *event_data)
+{
+    status_led_set_interval(status_led, STATUS_LED_CONNECTING_INTERVAL, true);
+}
 
 static void setup_init()
 {
@@ -22,26 +68,33 @@ static void setup_init()
     }
     ESP_ERROR_CHECK(err);
 
-    // Event loop
+    // System services
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    device_state_init();
 
     // Check double reset
     // NOTE this should be called as soon as possible, ideally right after nvs init
-    ESP_ERROR_CHECK(double_reset_start(&reconfigure, DOUBLE_RESET_DEFAULT_TIMEOUT));
+    bool reconfigure = false;
+    ESP_ERROR_CHECK_WITHOUT_ABORT(double_reset_start(&reconfigure, DOUBLE_RESET_DEFAULT_TIMEOUT));
+    if (reconfigure)
+    {
+        xEventGroupSetBits(device_state, STATE_BIT_RECONFIGURE);
+    }
+
+    // Load config
+    err = config.load();
+    if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND)
+    {
+        ESP_LOGE(TAG, "failed to load config: %d %s", err, esp_err_to_name(err));
+    }
 
     // Status LED
-    ESP_ERROR_CHECK_WITHOUT_ABORT(status_led_create_default());
-    ESP_ERROR_CHECK_WITHOUT_ABORT(status_led_set_interval(STATUS_LED_DEFAULT, 500, true));
+    ESP_ERROR_CHECK_WITHOUT_ABORT(status_led_create(config.status_led_pin, config.status_led_on_state, &status_led));
+    ESP_ERROR_CHECK_WITHOUT_ABORT(status_led_set_interval(status_led, STATUS_LED_CONNECTING_INTERVAL, true));
 
-    // Events
-    esp_event_handler_register(
-        WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval(STATUS_LED_DEFAULT, 500, true); }, NULL);
-    esp_event_handler_register(
-        WIFI_EVENT, WIFI_EVENT_STA_WPS_ER_SUCCESS, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval(STATUS_LED_DEFAULT, 500, true); }, NULL);
-    esp_event_handler_register(
-        WPS_CONFIG_EVENT, WPS_CONFIG_EVENT_START, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval(STATUS_LED_DEFAULT, 100, true); }, NULL);
-    esp_event_handler_register(
-        IP_EVENT, IP_EVENT_STA_GOT_IP, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval_for(STATUS_LED_DEFAULT, 200, false, 700, true); }, NULL);
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, status_led_wps_event_handler, nullptr, nullptr));
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(WPS_CONFIG_EVENT, WPS_CONFIG_EVENT_START, status_led_wps_event_handler, nullptr, nullptr));
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, status_led_disconnected_handler, nullptr, nullptr));
 }
 
 static void setup_devices()
@@ -49,67 +102,8 @@ static void setup_devices()
     // Custom devices and other init, that needs to be done before waiting for wifi connection
 }
 
-static void setup_wifi()
-{
-    // Get app info
-    esp_app_desc_t app_info = {};
-    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_ota_get_partition_description(esp_ota_get_running_partition(), &app_info));
-
-    // Initalize WiFi
-    ESP_ERROR_CHECK(esp_netif_init());
-    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
-    assert(sta_netif);
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_FLASH));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_MAX_MODEM));
-    ESP_ERROR_CHECK(esp_wifi_start());
-    ESP_ERROR_CHECK(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_STA, app_info.project_name));
-
-    ESP_ERROR_CHECK(wifi_reconnect_start()); // NOTE this must be called before connect, otherwise it might miss connected event
-
-    // Start WPS if WiFi is not configured, or reconfiguration was requested
-    if (!wifi_reconnect_is_ssid_stored() || reconfigure)
-    {
-        ESP_LOGI(TAG, "reconfigure request detected, starting WPS");
-        ESP_ERROR_CHECK(wps_config_start());
-        // Wait for WPS to finish
-        wifi_reconnect_wait_for_connection(WPS_CONFIG_TIMEOUT_MS);
-    }
-
-    // Connect now (needs to be called after WPS)
-    wifi_reconnect_resume();
-
-    // Wait for WiFi
-    ESP_LOGI(TAG, "waiting for wifi");
-    if (!wifi_reconnect_wait_for_connection(WIFI_RECONNECT_CONNECT_TIMEOUT_MS))
-    {
-        ESP_LOGE(TAG, "failed to connect to wifi!");
-        // NOTE either fallback into emergency operation mode, do nothing, restart..
-    }
-}
-
-static void setup_final()
+static void setup_final(const esp_app_desc_t *app_info)
 {
     // Ready
-    esp_app_desc_t app_info = {};
-    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_ota_get_partition_description(esp_ota_get_running_partition(), &app_info));
-    ESP_LOGI(TAG, "started %s %s", app_info.project_name, app_info.version);
-}
-
-static void run()
-{
-}
-
-extern "C" void app_main()
-{
-    // Setup
-    setup_init();
-    setup_devices();
-    setup_wifi();
-    setup_final();
-
-    // Run
-    run();
+    ESP_LOGI(TAG, "started %s %s", app_info->project_name, app_info->version);
 }
